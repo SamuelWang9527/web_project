30b92d3114c0f565fb2875bf0ead8ef717dd8064
Samuel <248282328@qq.com>
2026-05-01 10:43:26 +0800

    feat(client): migrate entry point to main.tsx

---
+++ client/src/main.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/locale/zh_CN'
+import App from './App'
+import './index.css'
+
+const root = document.getElementById('root')
+if (!root) throw new Error('Root element not found')
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+)
